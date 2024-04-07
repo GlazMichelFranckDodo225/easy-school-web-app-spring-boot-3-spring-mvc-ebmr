81faea14859ec294b7101c1e488cc87cd1142220
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-07 09:58:37 +0200

    Building Dynamic Content Using Thymeleaf

--- src/main/java/com/dgmf/controller/HomeController.java
+++ src/main/java/com/dgmf/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.dgmf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     // @RequestMapping("/home")
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Doe");
+
         return "home.html";
     }
 }
