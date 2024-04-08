cd77158bb33d4fb8bd1a6196e853942c2592e945
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-08 16:57:04 +0200

    Building Home Page of EazySchool Web Application

--- src/main/java/com/dgmf/controller/HomeController.java
+++ src/main/java/com/dgmf/controller/HomeController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     // @RequestMapping("/home")
-    @RequestMapping(value = {"", "/", "/home"})
+    /*@RequestMapping(value = {"", "/", "/home"})
     public String displayHomePage(Model model) {
         model.addAttribute("username", "Guttierez");
 
+        return "home.html";
+    }*/
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage() {
         return "home.html";
     }
 }
