bf01216a0cb7e0395af75657d44bdd7bf12b2a4a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-25 08:33:38 +0200

    Configure "permitAll()" inside Web App using Spring Security

---
+++ src/main/java/com/dgmf/config/AppSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.dgmf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class AppSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // To Authenticate all the Requests
+        // http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        // To Give Free Accesses to all the Requests
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+
+        // Return an Object of SecurityFilterChain Type
+        return http.build();
+    }
+}
