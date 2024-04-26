5c9f31bb6499bbc5b6e12d0ab66bc18559c02ce0
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-26 11:08:33 +0200

    Custom Security Configurations using Spring Security

--- src/main/java/com/dgmf/config/AppSecurityConfig.java
+++ src/main/java/com/dgmf/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.dgmf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,9 +17,21 @@ public class AppSecurityConfig {
         // To Unauthorized all Incoming Requests for all logged Users
         // http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
         // To Give Free Accesses to all the Requests
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        // http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        // Custom Security Configurations
+        http.authorizeHttpRequests(
+            auth -> auth
+                .requestMatchers("/", "/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("saveMsg").permitAll()
+                // .requestMatchers("/courses").authenticated()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+        )
+        .formLogin(withDefaults())
+        .httpBasic(withDefaults());
 
         // Return an Object of SecurityFilterChain Type
         return http.build();
