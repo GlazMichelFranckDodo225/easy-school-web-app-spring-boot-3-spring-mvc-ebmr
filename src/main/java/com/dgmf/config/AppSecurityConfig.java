ea3c311e9686b28321dad1de0d28a98bda5bc776
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-03 06:27:28 +0200

    Configure Multiple inMemory Users (Not Recommended for Production)

--- src/main/java/com/dgmf/config/AppSecurityConfig.java
+++ src/main/java/com/dgmf/config/AppSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,7 +30,9 @@ public class AppSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                     auth -> auth
+                        // .requestMatchers("dashboard").authenticated()
                         .requestMatchers("/", "/home").permitAll()
+                        // .requestMatchers("/", "/home").authenticated()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("saveMsg").permitAll()
@@ -40,4 +47,30 @@ public class AppSecurityConfig {
         // Return an Object of SecurityFilterChain Type
         return http.build();
     }
+
+    // PasswordEncoder Bean
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Spring Security In-Memory Authentication : https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/in-memory.html
+    // Configure Multiple inMemory Users (Not Recommended for Production)
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
 }
