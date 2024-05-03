21f5e23e85139ae6086a8b7d166d9962f290f45e
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-03 05:54:13 +0200

    CSRF Protection & CSRF Disable inside Spring Security Framework

--- src/main/java/com/dgmf/config/AppSecurityConfig.java
+++ src/main/java/com/dgmf/config/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,19 +20,22 @@ public class AppSecurityConfig {
         // To Give Free Accesses to all the Requests
         // http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
         // Custom Security Configurations
-        http.authorizeHttpRequests(
-            auth -> auth
-                .requestMatchers("/", "/home").permitAll()
-                .requestMatchers("/holidays/**").permitAll()
-                .requestMatchers("/contact").permitAll()
-                .requestMatchers("saveMsg").permitAll()
-                // .requestMatchers("/courses").authenticated()
-                .requestMatchers("/courses").permitAll()
-                .requestMatchers("/about").permitAll()
-                .requestMatchers("/assets/**").permitAll()
-        )
-        .formLogin(withDefaults())
-        .httpBasic(withDefaults());
+        http
+                // .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                    auth -> auth
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("saveMsg").permitAll()
+                        // .requestMatchers("/courses").authenticated()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
 
         // Return an Object of SecurityFilterChain Type
         return http.build();
