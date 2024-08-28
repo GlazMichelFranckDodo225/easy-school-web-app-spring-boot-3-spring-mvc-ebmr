405d7247e79a22e5e35254d8a1fd90c20fa6a2e4
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-28 09:34:47 +0200

    Fetch Data from DB to Profile Web Page

--- src/main/java/com/dgmf/controller/DashboardController.java
+++ src/main/java/com/dgmf/controller/DashboardController.java
@@ -1,6 +1,10 @@
 package com.dgmf.controller;
 
+import com.dgmf.entity.Person;
+import com.dgmf.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 public class DashboardController {
+    @Autowired
+    PersonRepository personRepository;
 
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication) {
-        model.addAttribute("username", authentication.getName());
-        model.addAttribute("roles", authentication.getAuthorities().toString());
-
-        // throw new RuntimeException("It's been a bad day!!");
+    public String displayDashboard(
+            Model model,
+            Authentication authentication,
+            HttpSession session // To Store Any Information Inside Session
+    ) {
+        Person person = personRepository.readByEmail(authentication.getName());
+        model.addAttribute("username", person.getName());
+        model.addAttribute(
+                "roles",
+                authentication.getAuthorities().toString()
+        );
+        // To Store Person Information Inside Session
+        session.setAttribute("loggedInPerson", person);
 
         return "dashboard.html";
     }
--- src/main/java/com/dgmf/controller/ProfileController.java
+++ src/main/java/com/dgmf/controller/ProfileController.java
@@ -1,18 +1,37 @@
 package com.dgmf.controller;
 
+import com.dgmf.entity.Person;
 import com.dgmf.entity.Profile;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.Period;
+
 @Slf4j
 @Controller
 public class ProfileController {
     @RequestMapping("/displayProfile")
-    public ModelAndView displayMessages(Model model) {
+    public ModelAndView displayMessages(Model model, HttpSession session) {
+        // Fetch Person DB Details from the Session
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        // Set Person Details to His Profile
         Profile profile = new Profile();
+        profile.setName(person.getName());
+        profile.setMobileNumber(person.getMobileNumber());
+        profile.setEmail(person.getEmail());
+        // Check If Address Details is Present into Person Object in Order to
+        // Set Also Address Details to His Profile
+        if(person.getAddress() !=null && person.getAddress().getAddressId()>0){
+            profile.setAddress1(person.getAddress().getAddress1());
+            profile.setAddress2(person.getAddress().getAddress2());
+            profile.setCity(person.getAddress().getCity());
+            profile.setState(person.getAddress().getState());
+            profile.setZipCode(person.getAddress().getZipCode());
+        }
         ModelAndView modelAndView = new ModelAndView("profile.html");
         // Send Profile Object to the UI
         modelAndView.addObject("profile", profile);
