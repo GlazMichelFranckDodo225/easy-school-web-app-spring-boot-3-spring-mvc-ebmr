4e48a4ca7855c843f7ce33b424f6724dfa0fbab9
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-01 09:55:35 +0200

    Save Address Data into DB from Profile Page

--- src/main/java/com/dgmf/controller/ProfileController.java
+++ src/main/java/com/dgmf/controller/ProfileController.java
@@ -1,11 +1,18 @@
 package com.dgmf.controller;
 
+import com.dgmf.entity.Address;
 import com.dgmf.entity.Person;
 import com.dgmf.entity.Profile;
+import com.dgmf.repository.PersonRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,7 +20,10 @@ import java.time.Period;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ProfileController {
+    private final PersonRepository personRepository;
+
     @RequestMapping("/displayProfile")
     public ModelAndView displayMessages(Model model, HttpSession session) {
         // Fetch Person DB Details from the Session
@@ -37,5 +47,39 @@ public class ProfileController {
         modelAndView.addObject("profile", profile);
 
         return modelAndView;
+    }
+
+    @PostMapping(value = "/updateProfile")
+    public String updateProfile(
+            @Valid @ModelAttribute("profile") Profile profile,
+            Errors errors,
+            HttpSession session
+    )
+    {
+        if(errors.hasErrors()){
+            // Return the Same Page
+            return "profile.html";
+        }
+        // Fetch Person Available into Session
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        // Update Person Object Details Using Profile  Object
+        person.setName(profile.getName());
+        person.setEmail(profile.getEmail());
+        person.setMobileNumber(profile.getMobileNumber());
+        if(person.getAddress() ==null || !(person.getAddress().getAddressId()>0)){
+            // Create New Empty Address
+            person.setAddress(new Address());
+        }
+        person.getAddress().setAddress1(profile.getAddress1());
+        person.getAddress().setAddress2(profile.getAddress2());
+        person.getAddress().setCity(profile.getCity());
+        person.getAddress().setState(profile.getState());
+        person.getAddress().setZipCode(profile.getZipCode());
+        Person savedPerson = personRepository.save(person);
+        // Set Person Session Data with the Latest Data
+        session.setAttribute("loggedInPerson", savedPerson);
+
+        // Show User Profile Page
+        return "redirect:/displayProfile";
     }
 }
