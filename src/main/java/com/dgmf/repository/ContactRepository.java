3f96e67ed6ea17a9db491336bb7a60ab41719c41
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-08 15:44:39 +0100

    Writing Custom Update Queries using @Query,@Modifying,@Transactional Annotations

--- src/main/java/com/dgmf/repository/ContactRepository.java
+++ src/main/java/com/dgmf/repository/ContactRepository.java
@@ -4,10 +4,11 @@ import com.dgmf.entity.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,4 +27,8 @@ public interface ContactRepository extends JpaRepository<Contact, Integer> {
             nativeQuery = true
     )
     Page<Contact> findByStatus(@Param("status") String status, Pageable pageable);
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.status = ?1 WHERE c.contactId = ?2")
+    int updateStatusById(String status, int id);
 }
