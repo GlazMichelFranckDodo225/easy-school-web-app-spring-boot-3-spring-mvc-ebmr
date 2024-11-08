35a8477db536c4cbc7144b40ea26ceffa207aca3
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-08 15:22:07 +0100

    Writing Custom Queries using @Query Annotation | JPQL Query

--- src/main/java/com/dgmf/repository/ContactRepository.java
+++ src/main/java/com/dgmf/repository/ContactRepository.java
@@ -4,7 +4,9 @@ import com.dgmf.entity.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,5 +20,7 @@ DB related operations and
 // public interface ContactRepository extends CrudRepository<Contact, Integer> {
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
     List<Contact> findByStatus(String status);
-    Page<Contact> findByStatus(String status, Pageable pageable);
+    @Query("SELECT c FROM Contact c WHERE c.status = :status")
+    //@Query(value = "SELECT * FROM contact_msg c WHERE c.status = :status",nativeQuery = true)
+    Page<Contact> findByStatus(@Param("status") String status, Pageable pageable);
 }
