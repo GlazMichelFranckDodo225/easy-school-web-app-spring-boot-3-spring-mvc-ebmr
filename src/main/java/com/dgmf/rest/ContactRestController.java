8f3c900cc4b95fd2dddfd74c079133f957caff4a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-27 06:36:19 +0100

    Save Operation using Rest Service & ResponseEntity

--- src/main/java/com/dgmf/rest/ContactRestController.java
+++ src/main/java/com/dgmf/rest/ContactRestController.java
@@ -1,9 +1,13 @@
 package com.dgmf.rest;
 
 import com.dgmf.entity.Contact;
+import com.dgmf.entity.Response;
 import com.dgmf.repository.ContactRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,5 +37,20 @@ public class ContactRestController {
         }else{
             return List.of();
         }
+    }
+
+    @PostMapping("/saveMsg")
+    // @ResponseBody
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact){
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
     }
 }
